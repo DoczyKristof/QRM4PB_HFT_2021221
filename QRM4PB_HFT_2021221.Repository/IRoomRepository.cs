7dc78779537ee00988150b81f307bbe3ef4d9c82
doczykristof <doczykristof@DESKTOP-MUH427P>
2021-11-21 20:43:03 +0100

    Created room repo interface and implemented it into the RoomRepository

---
+++ QRM4PB_HFT_2021221.Repository/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using QRM4PB_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRM4PB_HFT_2021221.Repository
+{
+    interface IRoomRepository
+    {
+        void Create(Room room);
+        Room ReadOne(int id); 
+        IQueryable<Room> ReadAll();
+        void Update(Room room);
+        void Delete(int id);
+    }
+}
