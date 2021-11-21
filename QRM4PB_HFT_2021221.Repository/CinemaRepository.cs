10f2a96053bdfcad812b90c0060a8484c426cdae
doczykristof <doczykristof@DESKTOP-MUH427P>
2021-11-21 20:37:16 +0100

    Created the cinema interface and implemented into CinemaRepo

--- QRM4PB_HFT_2021221.Repository/CinemaRepository.cs
+++ QRM4PB_HFT_2021221.Repository/CinemaRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace QRM4PB_HFT_2021221.Repository
 {
-    public class CinemaRepository
+    public class CinemaRepository : ICinemaRepository
     {
 
         CinemaDbContext context;
---
+++ QRM4PB_HFT_2021221.Repository/ICinemaRepository.cs
@@ -0,0 +1,18 @@
+﻿using QRM4PB_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRM4PB_HFT_2021221.Repository
+{
+    interface ICinemaRepository
+    {
+        void Create(Cinema cinema);
+        Cinema ReadOne(int id);
+        IQueryable<Cinema> ReadAll();
+        void Update(Cinema cinema);
+        void Delete(int id);
+    }
+}
