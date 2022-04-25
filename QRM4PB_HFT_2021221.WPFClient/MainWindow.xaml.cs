20de20ebf648182e766bd7796b9c2a7fd69a34b7
doczykristof <doczykristof@DESKTOP-MUH427P>
2022-04-25 20:49:19 +0200

    Created Main menu

---
+++ QRM4PB_HFT_2021221.WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace QRM4PB_HFT_2021221.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TimeService();
+        }
+
+        void TimeService()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            lb_date.Content = DateTime.Now.ToString("dddd, yyyy. MMM dd");
+            lb_clock.Content = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
